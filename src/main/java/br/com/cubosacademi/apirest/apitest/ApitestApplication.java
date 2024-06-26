c742f63ddc180495fd7e503ee45e800f4769f19f
Welton Vaz <weltonvaz@gmail.com>
2024-06-26 10:32:29 -0300

    Update: First commit

---
+++ src/main/java/br/com/cubosacademi/apirest/apitest/ApitestApplication.java
@@ -0,0 +1,13 @@
+package br.com.cubosacademi.apirest.apitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cubosacademi/apirest/apitest/ApitestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cubosacademi.apirest.apitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
